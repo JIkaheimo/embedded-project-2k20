4ad378c24eb9ac2754c40563b236deea7586bfc2
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-03-22 20:37:10 +0200

    Create function and setup for reading input from ADKey component.

--- controller/main.cpp
+++ controller/main.cpp
@@ -36,10 +36,50 @@ AnalogIn    joystickPinX(JOYSTICK_X);
 AnalogIn    joystickPinY(JOYSTICK_Y);
 DigitalIn   joystickPinButton(JOYSTICK_BTN, PullUp); // Use pullup for joystick button
 
-float   joystickX, joystickY;
-bool    joystickButton;
+float   joystickX = 0.0f, 
+        joystickY = 0.0f;
+
+bool    joystickButton = false;
 
 void readJoystick(float& x, float& y, bool& button);
+
+
+/*****************
+ * ADKEY CONFIGS *
+ *****************
+ *
+ * Button layout:
+ *
+ * A   B
+ * C D E
+ *
+ */
+const int A_MIN = 0;
+const int A_MAX = 5;
+
+const int B_MIN = 40;
+const int B_MAX = 50;
+
+const int C_MIN = 80;
+const int C_MAX = 99;
+
+const int D_MIN = 100;
+const int D_MAX = 150;
+
+const int E_MIN = 500;
+const int E_MAX = 550;
+
+const PinName ADKEY = A2;
+
+AnalogIn adkeyPin(ADKEY);
+
+void readADKey(bool&, bool&, bool&, bool&, bool&);
+
+bool    buttonA = false, 
+        buttonB = false, 
+        buttonC = false, 
+        buttonD = false, 
+        buttonE = false;
 
 
 void setup()
@@ -53,8 +93,19 @@ void loop()
     while (true)
     {
         readJoystick(joystickX, joystickY, joystickButton);
+        readADKey(buttonA, buttonB, buttonC, buttonD, buttonD);
 
-        device.printf("X: %.2f, Y: %.2f, JBTN: %s\n", joystickX, joystickY, joystickButton ? "true" : "false");
+        // Send controller state to serial device.
+        device.printf("X: %.2f, Y: %.2f, J: %s, A: %s, B: %s, C: %s, D: %s, E: %s\n", 
+            joystickX, 
+            joystickY, 
+            joystickButton  ? "true" : "false",
+            buttonA         ? "true" : "false",
+            buttonB         ? "true" : "false",
+            buttonC         ? "true" : "false",
+            buttonD         ? "true" : "false",
+            buttonE         ? "true" : "false"
+        );
     }
 }
 
@@ -77,4 +128,34 @@ void readJoystick(float& x, float& y, bool& button)
     x = 2 * (joystickPinX.read() - 0.5 + JOYSTICK_X_OFFSET);
     y = 2 * (joystickPinY.read() - 0.5 + JOYSTICK_Y_OFFSET);
     button = joystickPinButton.read() == 0;
+}
+
+
+void readADKey(
+    bool& buttonA, 
+    bool& buttonB, 
+    bool& buttonC, 
+    bool& buttonD, 
+    bool& buttonE
+)
+{
+    buttonA = false;
+    buttonB = false;
+    buttonC = false;
+    buttonD = false;
+    buttonE = false;
+
+    float reading = adkeyPin.read();
+
+    if (reading < 0.001) {
+        buttonA = true;
+    } else if (reading < 0.05) {
+        buttonB = true;
+    } else if (reading < 0.1) {
+        buttonC = true;
+    } else if (reading < 0.15) {
+        buttonD = true;
+    } else if (reading < 0.6) {
+        buttonE = true;
+    }
 }
