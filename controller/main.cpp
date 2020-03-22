4ba57584bb79f5627fa5a932210b663658331828
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-03-22 18:37:05 +0200

    Add joystick configurations.

--- controller/main.cpp
+++ controller/main.cpp
@@ -1,13 +1,26 @@
 #include "mbed.h"
 
+/* Serial configs */
 const int BAUD_RATE = 9600;
 
 Serial device(USBTX, USBRX);
+
+
+/* Joystick configs */
+const PinName JOYSTICK_X = A0;
+const PinName JOYSTICK_Y = A1;
+const PinName JOYSTICK_BTN = D8;
+
+AnalogIn JoystickX(JOYSTICK_X);
+AnalogIn JoystickY(JOYSTICK_Y);
+DigitalIn JoystickButton(JOYSTICK_BTN, PullUp);
+
 
 void setup()
 {
     device.baud(BAUD_RATE);
 }
+
 
 void loop()
 {
@@ -16,6 +29,7 @@ void loop()
         device.printf("Hello serial!\n");
     }
 }
+
 
 int main()
 {
