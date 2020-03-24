09765b1c6699d189ad2bc1db439d13e39ddf7e4d
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-03-24 15:49:30 +0200

    Update serial input to be processed in different thread.

--- controller-game/controller_game.py
+++ controller-game/controller_game.py
@@ -1,25 +1,61 @@
 import serial
 import serial.tools.list_ports
 import socketio
+import tkinter
 
-import asyncio
-import serial_asyncio
+import sys
 
-
-# Serial configs
-BAUD_RATE = 9600
+import threading
 
 
-async def serialWorker(eventLoop, port):
-    reader, writer = await serial_asyncio.open_serial_connection(url=port, baudrate=BAUD_RATE)
-    print("Connection established!")
+lock = threading.Lock()
 
-    # Read serial input
+window = tkinter.Frame()
+
+
+def initSerial(port):
+    """
+    Initializes a serial connection
+    with a controller device in the given port.
+    """
+
+    device = serial.Serial()
+
+    device.port = port
+    device.baudrate = 9600
+
+    device.bytesize = serial.EIGHTBITS
+    device.parity = serial.PARITY_NONE
+    device.stopbits = serial.STOPBITS_ONE
+
+    #device.timeout = 0
+    device.rtscts = False
+    device.dsrdtr = False
+    device.xonxoff = False
+
+    return device
+# initSerial end
+
+
+def read_serial(port):
+
+    device = initSerial(port)
+
+    buffer = []
+
+    device.open()
+
+    if not device.isOpen():
+        print("Error connecting to serial port {}.".format(port))
+        exit()
+
     while True:
-        msg = await reader.readuntil(b'\n')
+        msg = device.readline().decode('utf-8').rstrip()
+        print(msg)
 
 
 if __name__ == '__main__':
+
     nucleoPort = None
 
     # Get port number for Nucleo.
@@ -27,6 +63,24 @@ if __name__ == '__main__':
         if (identifier.startswith("STMicroelectronics")):
             nucleoPort = port
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(serialWorker(loop, port))
-    loop.close()
+    # Make sure nucleo is detected
+    if nucleoPort == None:
+        print("Could not detect connected Nucleo. Please make sure it is correctly connected...")
+        sys.exit()
+
+    # Create two threads as follows
+    try:
+        serialThread = threading.Thread(
+            target=read_serial, args=(nucleoPort, ))
+
+        serialThread.daemon = True
+        serialThread.start()
+    except:
+        print("Unable to open thread for reading serial data.")
+
+    # Main loop
+    try:
+
+        window.mainloop()
+    except KeyboardInterrupt:
+        exit()
